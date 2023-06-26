90c21e70c4d8bc246a17378b9b746c79419aba9b
christiansoh72 <christiansoh72@gmail.com>
2023-06-27 06:56:36 +0900

    Changed port to 80

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3000, (err) => {
+  server.listen(80, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log('> Ready on http://localhost:80');
   });
 });
